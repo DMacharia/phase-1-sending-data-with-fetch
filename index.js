d52e7bdac9dfb5fe16d102e405991e8724ed7f82
Daniel Macharia <danielhuios@gmail.com>
2022-08-26 19:40:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((resp) => resp.json())
+    .then((formData) => {
+      document.body.innerHTML = formData["id"];
+    })
+    .catch((error) => {
+      alert("Error!!");
+      document.body.innerHTML = error.message;
+    });
+}
